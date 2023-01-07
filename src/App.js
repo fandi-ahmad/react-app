8ca8cd46e5f25dd796137f954d60f87cdbef4a8a
fandi-ahmad <fandi4160@gmail.com>
2023-01-07 20:52:08 +0800

    create route

--- src/App.js
+++ src/App.js
@@ -1,64 +1,24 @@
 import React, { useState } from "react";
-import './App.css';
-import Header from './components/Header';
-import Profile from './components/Profile';
-import Navbar from './components/Navbar';
-import Btn from './components/Btn';
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/css/style.css'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Detail from "./pages/Detail";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Post from "./components/Post";
-import Product from "./components/Product";
+import Status from "./pages/Status";
 import ProtectedRoute from "./routes/ProtectedRoute";
+import Gate from "./pages/setting/Gate";
 
 function App() {
-    const navText = 'fandi ahmad'
-
-    const clicked = () => {
-        alert('button di klik')
-    }
-
-    const paragraf = () => {
-        return (
-            <div>
-                <i>the paragraf</i>
-                <marquee>mantapp</marquee>
-            </div>
-        )
-    }
-
-    const [getNav, setNav] = useState('')
-
-    const traceBtn = () => {
-        setNav('my kontak')
-    }
-
     return (
-        // <div>
-        //     <Navbar text={navText} navValue={getNav} />
-        //     <Btn clicked={clicked} />
-        //     <Btn clicked={traceBtn} />
-        // </div>
-
         <BrowserRouter>
-            <nav>
-                <Link to={"/"}>home</Link> | <Link to={"/login"}>login</Link>
-            </nav>
             <Routes>
-                <Route path="/" element={<Home/>} />
                 <Route path="/login" element={<Login/>} />
-                <Route path="/detail/:id"
-                    element={
-                        <ProtectedRoute>
-                            <Detail />
-                        </ProtectedRoute>
-                    }
-                >
-                    <Route path={"post"} element={<Post/>} />
-                    <Route path={"product"} element={<Product/>} />
-                </Route>
-                
+                <Route path="/home" element={<Home/>} />
+                <Route path="/detail/:id" element={<Detail />} />
+
+                <Route path="/" element={<Status/>} />
+                <Route path="/gate" element={<Gate/>} />
             </Routes>
         </BrowserRouter>
 
